18b6d25f46822b7261a4479304c7ef423f5631a8
Laboratorio <laboratorio@udla-ec.int>
2023-06-21 14:52:24 -0500

    Creacion de comentarios

--- Suma de matrices.c
+++ Suma de matrices.c
@@ -4,9 +4,11 @@
 
 int main()
 {
+    //Creacion de variables tanto para x, y y r como para los i y j para los for
     int x[3][2], y[3][2], r[3][2];
     int i, j;
 
+    //Creacion de for para meter numeros rand en cada fila
     srand(time(NULL));
     for (int i = 0; i < 3; i++)
     {
@@ -15,6 +17,7 @@ int main()
             x[i][j] = rand() % 10 + 1;
         }
     }
+    //Creacion de for para meter numeros rand en cada columna
     for (int i = 0; i < 3; i++)
     {
         for (int j = 0; j < 2; j++)
@@ -22,6 +25,7 @@ int main()
             y[i][j] = rand() % 10 + 1;
         }
     }
+    //Creacion de for para sumar cada fila con cada columna
     for (int i = 0; i < 3; i++)
     {
         for (int j = 0; j < 2; j++)
@@ -29,6 +33,8 @@ int main()
             r[i][j] = x[i][j] + y[i][j];
         }
     }
+
+    //For con un print f para imprimir cada resultado
 
     printf("\nResultado de la suma:\n");
     for (int i = 0; i < 3; i++)
